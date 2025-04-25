b520961beea50a98d624d7ef441e5b3b4448e0b1
Lucas Leon <luquitax78@gmail.com>
2025-04-25 20:48:49 -0300

    Creacion de estilo para la pagina de las tarjetas de presentacion

--- src/components/Integrantes.jsx
+++ src/components/Integrantes.jsx
@@ -1,32 +1,33 @@
 import React from 'react'
-
+import css from '../style/Integrantes.css'
 
 const Integrantes = ({ alumno1, alumnoL }) => {
     return (
-        <div>
-            <div>
+        <body>
+            <div className='contenedor'>
+                <div className='tarjeta'>
+                    <ul>
+                        <li>{alumno1.nombre}</li>
+                        <li>{alumno1.apellido}</li>
+                        <li>{alumno1.legajo}</li>
+                        <li><a href={alumno1.github}>Github</a></li>
+                        <li><figure><img src={require(`../resources/${alumno1.foto}`)} alt="foto alumno Juanma" /></figure></li>
+                    </ul>
+                </div>
 
-                <ul>
-                    <li>{alumno1.nombre}</li>
-                    <li>{alumno1.apellido}</li>
-                    <li>{alumno1.legajo}</li>
-                    <li><a href={alumno1.github}>Github</a></li>
-                    <li><figure><img src={require(`../resources/${alumno1.foto}`)} alt="foto alumno Juanma" /></figure></li>
-                </ul>
+                <div className='tarjeta'>
+                    <ul>
+                        <li>{alumnoL.nombre}</li>
+                        <li>{alumnoL.apellido}</li>
+                        <li>{alumnoL.legajo}</li>
+                        <li><a href={alumnoL.github}>Github</a></li>
+                        <li><figure><img src={require(`../resources/${alumnoL.foto}`)} alt="foto alumno Lucas" /></figure></li>
+                    </ul>
+                </div>
+
             </div>
 
-            <div>
-
-                <ul>
-                    <li>{alumnoL.nombre}</li>
-                    <li>{alumnoL.apellido}</li>
-                    <li>{alumnoL.legajo}</li>
-                    <li><a href={alumnoL.github}>Github</a></li>
-                    <li><figure><img src={require(`../resources/${alumnoL.foto}`)} alt="foto alumno Lucas" /></figure></li>
-                </ul>
-            </div>
-
-        </div>
+        </body>
 
     )
 }
