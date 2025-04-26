0096a45186eadd0040fb4af3186cd8d336f8a862
Christian Gabriel Diaz <christiangamu2201@gmail.com>
2025-04-26 13:10:28 -0300

    Hago el alumnoc para la verificacion y para que sea un nuevo integrante

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,14 +3,14 @@ import Formulario from './Formulario';
 import '../style/Main.css';
 import Integrantes from './Integrantes';
 
-const Main = ({ usuarioVerificacion, Verificado, alumno1, alumnoL,alumnog }) => {
+const Main = ({ usuarioVerificacion, Verificado, alumno1, alumnoL,alumnoc }) => {
 
     console.log(usuarioVerificacion);
     console.log(Verificado);
     return (
         <div>
             <main>
-                {usuarioVerificacion ? <Integrantes alumno1={alumno1} alumnoL={alumnoL}  alumnog={alumnog}/> : <Formulario Verificado={Verificado} />}
+                {usuarioVerificacion ? <Integrantes alumno1={alumno1} alumnoL={alumnoL}  alumnog={alumnoc}/> : <Formulario Verificado={Verificado} />}
             </main>
         </div>
     );
